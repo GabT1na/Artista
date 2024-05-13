0c0534cdd35132cf4cf3e4a779708913edb3a839
Gabrielly Cristina <167761882+GabT1na@users.noreply.github.com>
2024-05-13 15:10:13 +0000

    termino

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,22 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("•••••••••••••••••••••••••••••••••");
+System.out.println("•••••••••••••••••••••••••••••••••");
+    System.out.println(" ");
     System.out.println("|Banda: Evanescence|");
     System.out.println("|Membros atuais:Amy Lee, Emma Anzai, Will Hunt, Tim McCord e Troy McLawhorn|");
-    
-    System.out.println("••••••••••••••••••••••••••••••••••");
-
-System.out.println(" ");
-    
+    System.out.println(" ");
+System.out.println("••••••••••••••••••••••••••••••••••");
+System.out.println(" "); 
     System.out.println("Álbum: Fallen ");
     System.out.println("Ano de lançamento: 04 de Março de 2003");
     System.out.println("Gênero: Metal alternativo, nu metal, metal gótico");
     System.out.println("Duração: 44:20");
-    System.out.println("Fallen é o álbum de estreia da banda de rock americana Evanescence. Foi lançado em 4 de março de 2003 através da gravadora Wind-up Records. O álbum foi gravado em múltiplos estúdios, incluindo Conway Recording Studios em Hollywood, Califórnia");
+    System.out.println("Fallen é o álbum de estreia da banda de rock americana Evanescence. Foi lançado em 4 de março de 2003 através da gravadora Wind-up Records. O álbum foi gravado em múltiplos estúdios, incluindo Conway Recording Studios em Hollywood, Califórnia.");
 
       System.out.println(" ");
     
 System.out.println("•••••••••••••••••••••••••••••••••");
-    
+    System.out.println(" ");
     System.out.println("Músicas do Álbum:");
     System.out.println(" ");
     System.out.println("1.Going Under ----- 03:35");
@@ -42,6 +41,6 @@ System.out.println("••••••••••••••••••••
     System.out.println(" ");
     System.out.println("11.Whisper ----- 5:27");
     System.out.println("");
-    
+    System.out.println("•••••••••••••••••••••••••••••••••");
   }
 }
